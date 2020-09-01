c07a772a2b793723ca56b785e61d4fafddc38e6d
vandercoder <andikawilliam@outlook.com>
2020-09-01 22:27:46 +0700

    initial commit

---
+++ product.js
@@ -0,0 +1,27 @@
+var projectCards = new Vue({
+  el: '#products',
+  data: {
+    products : [
+      {
+        id : 1,
+        name : "Gawe Coffee",
+        price : "12k",
+      },
+      {
+        id : 2,
+        name : "Gawe Coffee Coconut",
+        price : "14k",
+      },
+      { 
+        id : 3, 
+        name : "Cafe Latte",
+        price : "14k",
+      },
+      { 
+        id : 4,
+        name : "Americano",
+        price : "8k",
+      },
+    ]    
+  }
+})
