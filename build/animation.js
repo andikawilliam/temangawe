9f8e9877f1bf14b83ed877a5af0a1a8466f3f0eb
vandercoder <andikawilliam@outlook.com>
2020-09-04 16:48:01 +0700

    added headline, changed background

--- build/animation.js
+++ build/animation.js
@@ -1,4 +1,28 @@
+
+let tween = gsap.to("#teman-gawe-cube", {
+  rotation: 360,
+  duation: 1,
+  ease: Sine.easeOut,
+});
+
+cube = document.querySelector('#teman-gawe-cube');
+cube.addEventListener("mouseover", function(el) {
+  tween.restart();
+});
+
+gsap.to("#headline", {
+  scrollTrigger: {
+    trigger: "#headline",
+    start: "80% 50%",
+    end: "bottom 20%",
+    id: "cube-id",
+    scrub: 0.5,
+  },
+  transform: 'scale(0.7)', opacity: 1, duration: 0.8,
+});
+
 gsap.registerPlugin(ScrollTrigger);
+
 
 gsap.utils.toArray(".text-animate").forEach(text => {
   gsap.to(text, {
@@ -8,6 +32,7 @@ gsap.utils.toArray(".text-animate").forEach(text => {
       end: "top 50%",
       id: "text-id",
       // markers: true,
+      scrub: 0.5,
       toggleActions: "play none none none",
     },
     y: '-2em', opacity: 1, duration: 0.8,
@@ -48,9 +73,10 @@ gsap.fromTo("#menu",
   {
     scrollTrigger: {
       trigger: "#menu",
-      start: "top 40%",
-      end: "top 20%",
-      markers: true,
+      start: "top 70%",
+      end: "top 40%",
+      scrub: 0.8,
+      // markers: true,
       toggleActions: "play none none none",
-  }, transform: 'translateX(0)', opacity: 1 }
+  }, transform: 'translateX(0)', opacity: 1, duration: 0.6, ease: Sine.easeOut }
 );
