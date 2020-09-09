aea0e307a48a25a294b0837b25794c25773f459e
vandercoder <andikawilliam@outlook.com>
2020-09-09 21:42:01 +0700

    initial commit version 2

--- build/index.js
+++ build/index.js
@@ -28,3 +28,36 @@ function myFunction() {
   var scrolled = (winScroll / height) * 100;
   document.getElementById("myBar").style.width = scrolled + "%";
 }
+
+
+const sections = document.querySelectorAll('section');
+const options = {
+  rootMargin: '-50px 0px -55%',
+  // threshold: 1.0
+};
+
+let observer = new IntersectionObserver(function (entries, self) {
+  entries.forEach(entry => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      intersectionHandler(entry); 
+    }
+  });
+}, options);
+
+sections.forEach(section => {
+  observer.observe(section);
+});
+
+function intersectionHandler(entry) {
+  const id = entry.target.id;
+  const currentlyActive = document.querySelector('.navlink.nav-active');
+  const shouldBeActive = document.querySelector('.navlink[data-ref=' + id + ']');
+
+  if (currentlyActive) {
+    currentlyActive.classList.remove('nav-active');
+  }
+  if (shouldBeActive) {
+    shouldBeActive.classList.add('nav-active');
+  }
+}
